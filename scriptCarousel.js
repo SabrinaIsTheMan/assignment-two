6a0e7d715bd3849ce8a7f3ba310bd02264fa4677
SabrinaIsTheMan <sabrina.yt.man@gmail.com>
2023-02-24 20:59:10 -0500

    add pseudocode, fixed error (commented out two lines by accident)

--- scriptCarousel.js
+++ scriptCarousel.js
@@ -15,8 +15,8 @@ const slideWidth = slides[0].getBoundingClientRect().width;
 // console.log(slideWidth);
 
 // slides currently overlapping in one place, arrange them next to one another
-    // slides[0].style.left = slideWidth * 0 + 'px';
-    // slides[1].style.left = slideWidth * 1 + 'px';
+slides[0].style.left = slideWidth * 0 + 'px';
+slides[1].style.left = slideWidth * 1 + 'px';
 
 // refactor for a lot of slides/cards
 const setSlidePosition = function(slide, index){
