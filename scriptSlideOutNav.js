040d35fb7fe11a3be5d054aa7fa701079d2ffa97
SabrinaIsTheMan <sabrina.yt.man@gmail.com>
2023-02-24 20:55:04 -0500

    add more pseudocode

--- scriptSlideOutNav.js
+++ scriptSlideOutNav.js
@@ -1,14 +1,17 @@
 // slideoutnav
 // reference https://www.youtube.com/watch?v=OFKBep95lb4
 
+// query button icons and the nav
 const menuOpenBtn = document.querySelector('.menuIcon');
 const menuCloseBtn = document.querySelector('.buttonContainer')
 const slideOutNav = document.querySelector('.slideOutNav');
 
+// when clicking open icon, nav will come out from the side
 menuOpenBtn.addEventListener('click', function(){
     slideOutNav.style.left = '0px';
 })
 
+// when clicking the close button, nav will return to where it was (out of frame)
 menuCloseBtn.addEventListener('click', function() {
     slideOutNav.style.left = '-500px';
 })
